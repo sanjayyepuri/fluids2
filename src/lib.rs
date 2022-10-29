09484e07e3bc3c2fd8ec17c3552c0ab649c57151
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2022-10-29 02:05:13 -0400

    add sliders to control the position of the torus

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,6 @@ extern {
 pub fn add(x: f32, y: f32) -> f32 {
     x + y
 }
+
+// the first step to create a pic simulation is to get a datastructure to represent the particles
+// we then need to ensure this datastructure is friendly to be read directly from memory in javascript
